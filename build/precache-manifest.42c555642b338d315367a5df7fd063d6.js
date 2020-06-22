5e796970e9b6eb71b8f74ec81c81d59deb6ad6c8
sscotto <37452558+sscotto@users.noreply.github.com>
2020-06-22 20:02:07 -0300

    CompiledCards

---
+++ build/precache-manifest.42c555642b338d315367a5df7fd063d6.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0149647dc27609dfe34b39f55b231ce",
+    "url": "/memo-test/index.html"
+  },
+  {
+    "revision": "694da2b290648aa35853",
+    "url": "/memo-test/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d73c8fa91d424567dc9a",
+    "url": "/memo-test/static/css/main.e8b90644.chunk.css"
+  },
+  {
+    "revision": "694da2b290648aa35853",
+    "url": "/memo-test/static/js/2.8d9956f6.chunk.js"
+  },
+  {
+    "revision": "b396b9c7cb89986c0edcd31803c9a394",
+    "url": "/memo-test/static/js/2.8d9956f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d73c8fa91d424567dc9a",
+    "url": "/memo-test/static/js/main.abc2c1c6.chunk.js"
+  },
+  {
+    "revision": "654765f5fd3b83f01d7f",
+    "url": "/memo-test/static/js/runtime-main.7d659c60.js"
+  },
+  {
+    "revision": "db1b6dbeb05ad3f23b4a1734ae2584f0",
+    "url": "/memo-test/static/media/1.db1b6dbe.png"
+  },
+  {
+    "revision": "9326c6cab0a0f85d58d3fc13cdf9716d",
+    "url": "/memo-test/static/media/1_gray.9326c6ca.png"
+  },
+  {
+    "revision": "4b9722d3c54c0f51550871fb0e459b95",
+    "url": "/memo-test/static/media/2.4b9722d3.png"
+  },
+  {
+    "revision": "662bd71ea2c0361d3f4dcac70bd16727",
+    "url": "/memo-test/static/media/2_gray.662bd71e.png"
+  },
+  {
+    "revision": "d793cad274f29de2f44d21d68bb31bee",
+    "url": "/memo-test/static/media/3.d793cad2.png"
+  },
+  {
+    "revision": "818cd7f585ccc300af083326f7227a31",
+    "url": "/memo-test/static/media/3_gray.818cd7f5.png"
+  },
+  {
+    "revision": "17faf19d9470bea190b8f18fe3a93eae",
+    "url": "/memo-test/static/media/4.17faf19d.png"
+  },
+  {
+    "revision": "62340972ce33cd314ec22ae1e485e4dd",
+    "url": "/memo-test/static/media/4_gray.62340972.png"
+  },
+  {
+    "revision": "3484cf6a088d1ee366539a989c1219d6",
+    "url": "/memo-test/static/media/5.3484cf6a.png"
+  },
+  {
+    "revision": "836206e1abf898b7c4851d56ab31d46e",
+    "url": "/memo-test/static/media/5_gray.836206e1.png"
+  },
+  {
+    "revision": "9cb12cd10626e6b980f3429682872b39",
+    "url": "/memo-test/static/media/6.9cb12cd1.png"
+  },
+  {
+    "revision": "40e18ce61d533d6cea43356f8037b582",
+    "url": "/memo-test/static/media/6_gray.40e18ce6.png"
+  },
+  {
+    "revision": "fbb0bdca92fb6d0027334e856944736d",
+    "url": "/memo-test/static/media/back_card.fbb0bdca.jpg"
+  }
+]);
