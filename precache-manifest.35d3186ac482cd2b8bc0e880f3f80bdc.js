87cd63d37a9a9e1fb8fdf1a4a7bf2d442a0c2dd2
sscotto <37452558+sscotto@users.noreply.github.com>
2020-06-22 19:43:36 -0300

    Updates

---
+++ precache-manifest.35d3186ac482cd2b8bc0e880f3f80bdc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12d5e7b328ca5e3407429734f71adc10",
+    "url": "/memo-test/index.html"
+  },
+  {
+    "revision": "fb129793a88eb72d36f1",
+    "url": "/memo-test/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "71894ac2b01632d51123",
+    "url": "/memo-test/static/css/main.e8b90644.chunk.css"
+  },
+  {
+    "revision": "fb129793a88eb72d36f1",
+    "url": "/memo-test/static/js/2.e6cda444.chunk.js"
+  },
+  {
+    "revision": "b396b9c7cb89986c0edcd31803c9a394",
+    "url": "/memo-test/static/js/2.e6cda444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71894ac2b01632d51123",
+    "url": "/memo-test/static/js/main.ca9a884c.chunk.js"
+  },
+  {
+    "revision": "654765f5fd3b83f01d7f",
+    "url": "/memo-test/static/js/runtime-main.7d659c60.js"
+  },
+  {
+    "revision": "fbb0bdca92fb6d0027334e856944736d",
+    "url": "/memo-test/static/media/back_card.fbb0bdca.jpg"
+  }
+]);
